186c2e2e6c9358ae69c193154cf37895c5927e5a
KirillPrushak <kirillprushak@gmail.com>
2025-06-16 00:11:46 +0300

    fix mobile responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,27 +9,27 @@ function Navbar() {
     { href: '#home', label: 'Home' },
     { href: '#about', label: 'About US' },
     { href: '#services', label: 'Our Services' },
-    { href: '#restimonias', label: 'Testimonials' },
+    { href: '#testimonials', label: 'Testimonials' },
   ];
 
   return (
     <nav
-      className="fixed top-0 left-0 right-0 bg-white/90 backdrop-blur-sm
-    z-50 border-b border-gray-100 shadow-sm"
+      className="fixed top-0 left-0 right-0 bg-white/90 backdrop-blur-sm z-50 border-b
+     border-gray-100 shadow-sm"
     >
       <div
-        className="w-full flex justify-between items-center container mx-auto 
-      px-4 sm:px-6 lg:px-8 md:h-20 h-16"
+        className="w-full flex justify-between items-center container mx-auto px-4 
+      sm:px-6 lg:px-8 md:h-20 h-16"
       >
         {/* logo */}
         <div className="flex items-center gap-1 cursor-pointer">
           <div
-            className="w-4 h-4 bg-blue-500 rounded-full opacity-75 
-          hover:opacity-100 transition-opacity"
+            className="w-4 h-4 bg-blue-500 rounded-full opacity-75 hover:opacity-100 
+          transition-opacity"
           ></div>
           <div
-            className="w-4 h-4 bg-red-500 -ml-2 rounded-full opacity-100 
-          hover:opacity-75 transition-opacity"
+            className="w-4 h-4 bg-red-500 -ml-2 rounded-full opacity-100 hover:opacity-75 
+          transition-opacity"
           ></div>
         </div>
 
@@ -40,9 +40,7 @@ function Navbar() {
               key={index}
               onClick={() => setActiveLink(link.href)}
               href={link.href}
-              className={`text-sm font-medium relative after:absolute after:bottom-0 
-              after:left-0 after:h-0.5 after:w-0 hover:after:w-full after:bg-blue-600
-              after:transition-all ${
+              className={`text-sm font-medium relative after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 hover:after:w-full after:bg-blue-600 after:transition-all ${
                 activeLink === link.href
                   ? 'text-blue-600'
                   : 'text-gray-600 hover:text-gray-900'
@@ -61,36 +59,6 @@ function Navbar() {
           {isMenuOpen ? <HiX className="size-6" /> : <HiMenu />}
         </button>
 
-        {/* mobile menu items */}
-        {isMenuOpen && (
-          <div className="md:hidden bg-white border-t border-gray-100 py-4">
-            <div className="container mx-auto px-4 space-y-4">
-              {navLinks.map((link, index) => (
-                <a
-                  key={index}
-                  onClick={() => {
-                    setActiveLink(link.href);
-                    setIsMenuOpen(false);
-                  }}
-                  className={`block text-sm font-medium py-2
-                  ${
-                    activeLink === link.href
-                      ? 'text-blue-600'
-                      : 'text-gray-600 hover:text-gray-900'
-                  }`}
-                  href={link.href}
-                >
-                  {link.label}
-                </a>
-              ))}
-
-              <button className="w-full bg-blue-600 text-white px-6 py-2.5 rounded-lg hover:bg-blue-700 text-sm font-medium transition-all hover:shadow-lg hover:shadow-blue-100">
-                <a href="#newsletter">Get in touch</a>
-              </button>
-            </div>
-          </div>
-        )}
-
         {/* get in touch btn */}
         <button
           className="hidden md:block bg-blue-600 text-white px-6 py-2.5 
@@ -102,6 +70,36 @@ function Navbar() {
 
         {/* mobile menu */}
       </div>
+
+      {/* mobile menu items */}
+      {isMenuOpen && (
+        <div className="md:hidden bg-white border-t border-gray-100 py-4">
+          <div className="container mx-auto px-4 space-y-4">
+            {navLinks.map((link, index) => (
+              <a
+                key={index}
+                onClick={() => {
+                  setActiveLink(link.href);
+                  setIsMenuOpen(false);
+                }}
+                className={`block text-sm font-medium py-2
+                  ${
+                    activeLink === link.href
+                      ? 'text-blue-600'
+                      : 'text-gray-600 hover:text-gray-900'
+                  }`}
+                href={link.href}
+              >
+                {link.label}
+              </a>
+            ))}
+
+            <button className="w-full bg-blue-600 text-white px-6 py-2.5 rounded-lg hover:bg-blue-700 text-sm font-medium transition-all hover:shadow-lg hover:shadow-blue-100">
+              <a href="#newsletter">Get in touch</a>
+            </button>
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
